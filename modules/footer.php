57971fad48efeb2dc9de23eb2d6c8bf4c381feca
Calvin <Calvin@10.0.0.7>
2014-06-12 07:11:37 -0700

    Test 1

--- modules/footer.php
+++ modules/footer.php
@@ -1,7 +1,7 @@
-<div style="background-color:#000033;width:100%;height:200px;padding-right:5%;clear:both;">
+<div style="background-color:#000033; height:250px;">
     
-<a href='INSERT LINK'><img id='portfolio' src='/media/portfolio.png' style='float:left;margin-left:15%;padding:10px; height:150px;width:150px;' alt="My Portfolio"></a>
-<a href='INSERT LINK'><img id='home' src='/media/home.png' style='float:left;padding:10px;height:150px;width:150px;' alt="Home"></a>
-<a href='INSERT LINK'><img id ='news' src='/media/news.png' style='float:left;padding:10px;height:150px;width:150px;'alt="Market News"></a>
+<a href='http://www.mystocksapp.com/portfolio.php'><img id='portfolio' src='/media/portfolio.png' style='float:left;margin-left:15%;padding:55px 10px 10px 10px; height:100px;width:100px;' alt="My Portfolio"></a>
+<a href='http://www.mystocksapp.com/index.php'><img id='home' src='/media/home.png' style='float:left;padding:25px 10px 10px 10px;height:150px;width:150px;' alt="Home"></a>
+<a href='http://www.mystocksapp.com/news.php'><img id ='news' src='/media/news.png' style='float:left;padding:30px 10px 10px 10px;height:150px;width:150px;'alt="Market News"></a>
 
 </div>
--- modules/header.php
+++ modules/header.php
@@ -1,2 +1,2 @@
-<div id='heading'><a href='http://www.mystocksapp.com/index.php'><img id='main'src="http://www.mystocksapp.com/media/mystocks.png" alt="My Stocks" style="padding-left:25%;"><a></div>
+<div id='heading'><a href='http://www.mystocksapp.com/index.php'><img id='main'src="http://www.mystocksapp.com/media/mystocks.png" alt="My Stocks" style="padding-left:0px;"><a></div>
             
