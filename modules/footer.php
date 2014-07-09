15959d4ecb9f8473569d9bc81ff558a09015e9fa
Colby Griffiths <ColbyGriffiths@live.com>
2014-07-09 01:57:22 -0700

    fixed some html/css error I made on last update

--- modules/footer.php
+++ modules/footer.php
@@ -1,10 +1,10 @@
-<footer class="footer">
+<footer class="foot">
     
 	<a href='http://www.mystocksapp.com/portfolio.php'>
 		<img id='portfolio' src='/media/portfolio.png' style='float:left;margin-left:15%;padding:55px 10px 10px 10px; height:100px;width:100px;' alt="My Portfolio"></a>
 	<a href='http://www.mystocksapp.com/index.php'>
 		<img id ='home' src='/media/home.png' style='float:left;padding:25px 10px 10px 10px;height:150px;width:150px;' alt="Home"></a>
 	<a href='http://www.mystocksapp.com/news.php'>
-		<img id ='news' src='/media/news.png' style='float:left;padding:30px 10px 10px 10px;height:150px;width:150px;'alt="Market News"></a>
+		<img id ='news' src='/media/news.png' style='float:left;padding:30px 10px 10px 10px;height:150px;width:150px;' alt="Market News"></a>
 
 </footer>
